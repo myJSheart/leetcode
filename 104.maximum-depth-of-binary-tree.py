4e3b8889bc113770905866bfa37354ad0267cb9a
Chenxu Zhao <zhaochenxu001@gmail.com>
2020-10-05 23:34:46 +1100

    add: 104 Maximum Depth of Binary Tree - v2

--- 104.maximum-depth-of-binary-tree.py
+++ 104.maximum-depth-of-binary-tree.py
@@ -11,23 +11,41 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+"""Version 1"""
+# class Solution:
+#     def maxDepth(self, root: TreeNode) -> int:
+#         if root is None:
+#             return 0
+
+#         max_depth = 1
+#         max_left = 0
+#         max_right = 0
+#         if root.left:
+#             max_left = self.maxDepth(root.left)
+#         if root.right:
+#             max_right = self.maxDepth(root.right)
+
+#         if max_left > max_right:
+#             max_depth += max_left
+#         else:
+#             max_depth += max_right
+
+#         return max_depth
+
+"""Version 2"""
+
+
 class Solution:
     def maxDepth(self, root: TreeNode) -> int:
         if root is None:
             return 0
 
-        max_depth = 1
-        max_left = 0
-        max_right = 0
-        if root.left:
-            max_left = self.maxDepth(root.left)
-        if root.right:
-            max_right = self.maxDepth(root.right)
+        max_left = self.maxDepth(root.left)
+        max_right = self.maxDepth(root.right)
 
         if max_left > max_right:
-            max_depth += max_left
-        else:
-            max_depth += max_right
+            return max_left + 1
 
-        return max_depth
+        return max_right + 1
 # @lc code=end
